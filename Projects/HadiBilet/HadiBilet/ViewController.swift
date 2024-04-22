036152211926d1619b6069ab0e487431a8af08f6
yunusdvp <yunusemre7033@gmail.com>
2024-04-22 09:40:11 +0300

    Project created

---
+++ Projects/HadiBilet/HadiBilet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HadiBilet
+//
+//  Created by Yunus Emre ÖZŞAHİN on 22.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
